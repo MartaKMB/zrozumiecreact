e783f0da8d8438b83142945f629d92bc17c745fa
MartaKMB <muszkamarta@interia.pl>
2025-10-16 16:52:06 +0200

    style: header

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,7 +1,15 @@
-import './App.css'
+import styles from './App.module.css'
 
 function App() {
- return <div>TODO</div>
+ return <div className={styles.container}>
+    <header className={styles.header}>
+        <div>
+            <h1>Do zrobienia</h1>
+            <h2>5 zadań</h2>
+        </div>
+        <button className={styles.button}>+</button>
+    </header>
+ </div>
 }
 
 export default App
