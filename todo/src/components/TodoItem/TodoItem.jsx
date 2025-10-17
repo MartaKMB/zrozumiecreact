c5b84f23155edc394889b2d684d90a5f586f7a31
MartaKMB <muszkamarta@interia.pl>
2025-10-17 19:50:03 +0200

    feat: done todo

--- todo/src/components/TodoItem/TodoItem.jsx
+++ todo/src/components/TodoItem/TodoItem.jsx
@@ -1,11 +1,11 @@
 import { Button } from "../Button/Button";
 import styles from "./TodoItem.module.css";
 
-export function TodoItem({ name, done, onDeleteButtonClick }) {
+export function TodoItem({ name, done, onDeleteButtonClick, onDoneButtonClick }) {
   return (
     <li className={styles.item}>
       <span className={`${styles.name} ${done ? styles.done : ''}`}>{name}</span>
-      {!done && <Button>Zrobione</Button>}
+      {!done && <Button onClick={onDoneButtonClick}>Zrobione</Button>}
       <Button onClick={onDeleteButtonClick}>Usuń</Button>
     </li>
   );
