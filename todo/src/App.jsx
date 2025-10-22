72436f68b015c87a785fb51555fc4eb573afbaea
MartaKMB <muszkamarta@interia.pl>
2025-10-22 09:54:11 +0200

    perf: reducer add delete and finish

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,61 +1,66 @@
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
 import styles from './App.module.css';
 import { Form } from './components/Form/Form';
 import { TodoItem } from './components/TodoItem/TodoItem';
 import { getSubheading } from './utils/getSubheading';
 
+function todosReducer(state, action) {
+  console.log("reducer!", action.type);
+  if (action.type === "delete") return state.filter((todo) => todo.id !== action.id);
+  if (action.type === "finish") return state.map((todo) => {
+        if (todo.id !== action.id) return todo;
+        return { ...todo, done: true };
+      });
+  if (action.type === "add") return [...state,
+      {
+        name: action.newTodo,
+        done: false,
+        id: Math.random(),
+      },]
+}
+
 function App() {
   const [isFormShown, setIsFormShown] = useState(false);
-  const [todos, setTodos] = useState([
+
+  const [todos, dispatch] = useReducer(todosReducer, [
     { name: 'zapłacić rachunki', done: false, id: 1 },
     { name: 'wyrzucić śmieci', done: true, id: 2 },
   ]);
+
   const subheading = getSubheading(todos.length);
 
   function addItem(newTodo) {
-    setTodos((prevTodos) => [
-      ...prevTodos,
-      {
-        name: newTodo,
-        done: false,
-        id: Math.random(),
-      },
-    ]);
+    dispatch({type: "add", newTodo});
     setIsFormShown(false);
   }
 
   function deleteItem(id) {
-    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    dispatch({type: "delete", id});
   }
 
   function finishItem(id) {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) => {
-        if (todo.id !== id) return todo;
-        return { ...todo, done: true };
-      })
-    );
+    dispatch({type: "finish", id});
   }
 
   function moveItem(direction, index) {
     const newIndex = index + direction;
     if (newIndex < 0 || newIndex >= todos.length) return; // poza zakresem
 
-    setTodos((prevTodos) => {
-      const newItems = [...prevTodos];
-      const [movedItem] = newItems.splice(index, 1);
-      newItems.splice(newIndex, 0, movedItem);
-      return newItems;
-    });
+    // setTodos((prevTodos) => {
+    //   const newItems = [...prevTodos];
+    //   const [movedItem] = newItems.splice(index, 1);
+    //   newItems.splice(newIndex, 0, movedItem);
+    //   return newItems;
+    // });
   }
 
   function editItem(id, newName) {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) => {
-        if (todo.id !== id) return todo;
-        return { ...todo, name: newName };
-      })
-    );
+    // setTodos((prevTodos) =>
+    //   prevTodos.map((todo) => {
+    //     if (todo.id !== id) return todo;
+    //     return { ...todo, name: newName };
+    //   })
+    // );
   }
 
   return (
