54c1f4013fde8182e6644913ab6d502f2287c5a3
MartaKMB <muszkamarta@interia.pl>
2025-10-23 16:36:21 +0200

    feat: free devilery in cart

--- shopApp/front-end/src/components/CartSummary/CartSummary.jsx
+++ shopApp/front-end/src/components/CartSummary/CartSummary.jsx
@@ -3,25 +3,37 @@ import CAR_ICON from "../../assets/car.svg";
 import { FullWidthButton } from "../FullWidthButton/FullWidthButton";
 
 export function CartSummary({ products }) {
+  const deliveryCost = 49;
+  const minSummaryFreeDelivery = 500;
+
+  let sum = 0;
+  products.forEach((product) => {
+    sum += product.pricePLN;
+  });
+
+  const totalCost = sum > minSummaryFreeDelivery ? sum : sum + deliveryCost;
+
   return (
     <div className={styles.cartSummary}>
       <h2>Podsumowanie</h2>
       <div className={styles.cartRow}>
         <p>Wartość produktów: </p>
-        <p>398zł</p>
+        <p>{sum}zł</p>
       </div>
       <div className={styles.cartRow}>
         <p>Koszt dostawy:</p>
-        <p>49zł</p>
+        <p>{sum > minSummaryFreeDelivery ? "0" : "49"}zł</p>
       </div>
       <div className={styles.cartRow}>
         <p>Do zapłaty</p>
-        <p>447zł</p>
+        <p>{totalCost}zł</p>
       </div>
       <FullWidthButton isBlack={true}>Do kasy</FullWidthButton>
       <div className={styles.cartSummaryRow}>
         <img src={CAR_ICON} />
-        <p className={styles.deliveryInfo}>Darmowa dostawa od 500zł</p>
+        <p className={styles.deliveryInfo}>
+          Darmowa dostawa od {minSummaryFreeDelivery}zł
+        </p>
       </div>
     </div>
   );
