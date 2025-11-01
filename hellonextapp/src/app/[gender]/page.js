4df9e5bdd895a893db9e2467f7e99a7bd0f3bfe5
MartaKMB <muszkamarta@interia.pl>
2025-11-01 11:34:04 +0100

    build: static site generators

--- hellonextapp/src/app/[gender]/page.js
+++ hellonextapp/src/app/[gender]/page.js
@@ -2,3 +2,17 @@ export default async function DynamicRoot({params}) {
     const {gender} = await params;
     return <h2>{gender}</h2>;
 }
+
+export async function generateStaticParams() {
+    return [
+        {
+            gender: "kobieta"
+        },
+        {
+            gender: "mezczyzna"
+        },
+        {
+            gender: "dziecko"
+        }
+    ]
+}
