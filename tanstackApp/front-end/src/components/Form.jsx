6515f26f8468b7384cd1119d56418f4c53ad21e2
MartaKMB <muszkamarta@interia.pl>
2025-11-14 20:49:43 +0100

    feat: post data with tanstack

--- tanstackApp/front-end/src/components/Form.jsx
+++ tanstackApp/front-end/src/components/Form.jsx
@@ -1,46 +1,86 @@
-import { useState } from "react";
-import "./Form.css";
+import { useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import './Form.css';
 
 export function Form() {
-    const [isFormShown, setIsFormShown] = useState(false);
+  const [isFormShown, setIsFormShown] = useState(false);
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [age, setAge] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [age, setAge] = useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log({ name, email, age });
-    };
+  const queryClient = useQueryClient();
 
-    return (
-        <div>
-            {isFormShown ? (
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <input
-                            type="text"
-                            placeholder="Imię"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <input
-                            placeholder="Wiek"
-                            value={age}
-                            onChange={(e) => setAge(e.target.value)}
-                        />
-                    </div>
-                    <button type="submit">Dodaj</button>
-                </form>
-            ) : (
-                <button onClick={() => setIsFormShown(true)}>Dodaj</button>
-            )}
-        </div>
+  const { mutate, isPending } = useMutation({
+    mutationFn: (newPerson) =>
+      fetch('http://localhost:3000/people', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newPerson),
+      }).then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error('Błąd dodawania osoby');
+      }),
+    onSuccess: (res) => {
+      const peopleData = queryClient.getQueryData(['people']);
+      queryClient.setQueryData(['people'], [...peopleData, res]);
+      queryClient.invalidateQueries({ queryKey: ['people'] });
+    },
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log({ name, email, age });
+    mutate(
+      { name, email, age: Number(age) },
+      {
+        onError: (error) => alert(error),
+        onSettled: () => {
+          setIsFormShown(false);
+          setName('');
+          setEmail('');
+          setAge('');
+        },
+      }
     );
+  };
+
+  if (isPending) {
+    return <div>Zapisywanie...</div>;
+  }
+
+  return (
+    <div>
+      {isFormShown ? (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type='text'
+              placeholder='Imię'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type='email'
+              placeholder='Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              placeholder='Wiek'
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
+          <button type='submit'>Dodaj</button>
+        </form>
+      ) : (
+        <button onClick={() => setIsFormShown(true)}>Dodaj</button>
+      )}
+    </div>
+  );
 }
